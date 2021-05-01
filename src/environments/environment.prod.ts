7eb95a75661f406a25746e51581fa6a1bd7c8e73
sMARCHz <mart_nattanon@hotmail.com>
2021-05-01 14:27:18 +0700

    create service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl1: "https://secure-reef-62579.herokuapp.com",
+  baseUrl2: "https://shielded-brook-09988.herokuapp.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl1: "https://secure-reef-62579.herokuapp.com",
+  baseUrl2: "https://shielded-brook-09988.herokuapp.com"
 };
 
 /*
